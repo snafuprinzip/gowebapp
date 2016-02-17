766a6fd2416c54e1141c3eb90e91141f29c2f46f
Michael Leimenmeier <mleimenmeier@me.com>
2016-02-17 11:18:44 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		RenderTemplate(w, r, "index/home", nil)
+	})
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
+
+	log.Fatal(http.ListenAndServe(":3000", mux))
+
+}
